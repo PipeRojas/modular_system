a3569b8330e59d242537f325a2e58ccc631747d0
Julian Devia <julian.devia@mail.escuelaing.edu.co>
2016-12-27 23:20:31 -0500

    inicializacion

---
+++ src/main/java/edu/eci/researchgroup/controller/SecurityController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.researchgroup.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+/**
+ *
+ * @author Julian David Devia Serna
+ */
+public class SecurityController {
+    @RequestMapping("/app/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+    @RequestMapping(value = "/logout", method = RequestMethod.POST)
+    public ResponseEntity<?> logout(Principal user) {
+        return new ResponseEntity<Object>(HttpStatus.ACCEPTED);
+    }
+}
